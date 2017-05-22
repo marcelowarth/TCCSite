7041e5929f2fa6959a4482de76d5475ee955f2c9
Unknown <marcelowarth@gmail.com>
2017-05-22 16:21:24 -0300

    site

---
+++ code/TCCSite/TCCSite/Controllers/SiteController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TCCSite.Controllers
+{
+    public class SiteController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /*
+        public ActionResult FichaTecnica()
+        {
+            return View();
+        }
+
+        public ActionResult Sobre()
+        {
+            var eu = new SobreMimModel()
+            {
+                Nome = "Sasuke",
+                Nascimento = "08 de junho de 1992",
+                Contato = "marcelowarth@gmail.com",
+                Escolaridade = "Superior Incompleto",
+                InstituicaoEnsino = "Feevale",
+                Semestre = 8
+            };
+            return View(eu);
+        }
+        */
+    }
+}
